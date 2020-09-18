9a306f33d869621645057d016300514f6193f933
SlavicPotato <sp@eth.si>
2020-09-18 09:28:09 +0200

    Enumeration stuff

--- ext/IMisc.h
+++ ext/IMisc.h
@@ -9,3 +9,19 @@ namespace Enum
         return static_cast<typename std::underlying_type<T>::type>(e);
     }
 }
+
+#define DEFINE_ENUM_CLASS_BITWISE(x) \
+    inline constexpr x operator|(x a_lhs, x a_rhs) noexcept { \
+        return static_cast<x>(Enum::Underlying(a_lhs) | Enum::Underlying(a_rhs)); } \
+    inline constexpr x operator&(x a_lhs, x a_rhs) noexcept { \
+        return static_cast<x>(Enum::Underlying(a_lhs) & Enum::Underlying(a_rhs)); } \
+    inline constexpr x operator^(x a_lhs, x a_rhs) noexcept { \
+        return static_cast<x>(Enum::Underlying(a_lhs) ^ Enum::Underlying(a_rhs)); } \
+    inline constexpr x operator~(x a_lhs) noexcept { \
+        return static_cast<x>(~Enum::Underlying(a_lhs)); } \
+    inline constexpr x& operator|=(x &a_lhs, x a_rhs) noexcept { \
+        a_lhs = static_cast<x>(Enum::Underlying(a_lhs) | Enum::Underlying(a_rhs)); return a_lhs; } \
+    inline constexpr x& operator&=(x &a_lhs, x a_rhs) noexcept { \
+        a_lhs = static_cast<x>(Enum::Underlying(a_lhs) & Enum::Underlying(a_rhs)); return a_lhs; } \
+    inline constexpr x& operator^=(x &a_lhs, x a_rhs) noexcept { \
+        a_lhs = static_cast<x>(Enum::Underlying(a_lhs) ^ Enum::Underlying(a_rhs)); return a_lhs; }

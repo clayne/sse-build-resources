285e439b1aee3d4f4e8627019c8a9c1c5ebe099e
SlavicPotato <sp@eth.si>
2020-09-28 23:25:06 +0200

    Update

--- ext/Patching.cpp
+++ ext/Patching.cpp
@@ -5,17 +5,17 @@ namespace Patching
 	void safe_write(uintptr_t addr, const void* data, size_t len)
 	{
 		DWORD oldProtect;
-		ASSERT(VirtualProtect(reinterpret_cast<void*>(addr), len, PAGE_EXECUTE_READWRITE, &oldProtect));
+		_assert(VirtualProtect(reinterpret_cast<void*>(addr), len, PAGE_EXECUTE_READWRITE, &oldProtect));
 		memcpy(reinterpret_cast<void*>(addr), data, len);
-		ASSERT(VirtualProtect(reinterpret_cast<void*>(addr), len, oldProtect, &oldProtect));
+		_assert(VirtualProtect(reinterpret_cast<void*>(addr), len, oldProtect, &oldProtect));
 	}
 
 	void safe_memset(uintptr_t addr, int val, size_t len)
 	{
 		DWORD oldProtect;
-		ASSERT(VirtualProtect(reinterpret_cast<void*>(addr), len, PAGE_EXECUTE_READWRITE, &oldProtect));
+		_assert(VirtualProtect(reinterpret_cast<void*>(addr), len, PAGE_EXECUTE_READWRITE, &oldProtect));
 		memset(reinterpret_cast<void*>(addr), val, len);
-		ASSERT(VirtualProtect(reinterpret_cast<void*>(addr), len, oldProtect, &oldProtect));
+		_assert(VirtualProtect(reinterpret_cast<void*>(addr), len, oldProtect, &oldProtect));
 	}
 
 	bool validate_mem(uintptr_t addr, const void* data, size_t len)

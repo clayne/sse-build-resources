3f24c03cea6f88d848ea359caabfd9b557c406a5
SlavicPotato <sp@eth.si>
2021-02-21 01:44:08 +0100

    update

--- ext/ICommon.h
+++ ext/ICommon.h
@@ -2,10 +2,10 @@
 
 #define FN_NAMEPROC(x) \
 virtual const char* ModuleName() const noexcept { return x; }; \
-virtual const char* LogPrefix() const noexcept { return "["  x  "]"; }; \
-virtual const char* LogPrefixWarning() const noexcept { return "<WARNING> ["  x  "]"; }; \
-virtual const char* LogPrefixError() const noexcept { return "<ERROR> ["  x  "]"; }; \
-virtual const char* LogPrefixFatal() const noexcept { return "<FATAL> ["  x  "]"; }; 
+virtual const char* LogPrefix() const noexcept { return "["  x  "] "; }; \
+virtual const char* LogPrefixWarning() const noexcept { return "<WARNING> ["  x  "] "; }; \
+virtual const char* LogPrefixError() const noexcept { return "<ERROR> ["  x  "] "; }; \
+virtual const char* LogPrefixFatal() const noexcept { return "<FATAL> ["  x  "] "; }; 
 
 #define SKMP_FORCEINLINE __forceinline
 #define SKMP_NOINLINE __declspec(noinline)
